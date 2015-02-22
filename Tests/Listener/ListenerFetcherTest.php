7f9d5aaed942be69e058dc7b99d4e802304482c0
Eduardo Gulias Davis <me@egulias.com>
2015-02-22 17:05:59 +0100

    Use mockery to fix tests due to a new interface for container builder

--- Tests/Listener/ListenerFetcherTest.php
+++ Tests/Listener/ListenerFetcherTest.php
@@ -10,8 +10,10 @@
 namespace Egulias\ListenersDebug\Tests\Listener;
 
 use PHPUnit_Framework_TestCase;
+use Mockery as m;
 
 use Egulias\ListenersDebug\Listener\ListenerFetcher;
+
 
 /**
  * ListenerFetcherTest
@@ -25,29 +27,17 @@ class ListenerFetcherTest extends PHPUnit_Framework_TestCase
      */
     public function testFetchListeners($eventData)
     {
-        $defMock = $this->getMockBuilder('Symfony\Component\DependencyInjection\Definition')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $defMock->expects($this->once())->method('getTags')->will(
-            $this->returnValue(array($eventData['name'] => array()))
-        );
-        $defMock->expects($this->once())->method('isPublic')->will($this->returnValue(true));
-        $defMock->expects($this->any())
-            ->method('getClass')
-            ->will($this->returnValue('Egulias\ListenersDebug\Tests\Listener\Listener'));
+        $defMock = m::mock('Symfony\Component\DependencyInjection\Definition');
+        $defMock->shouldReceive('getTags')->once()->andReturn(array($eventData['name'] => array()));
+        $defMock->shouldReceive('isPublic')->once()->andReturn(true);
+        $defMock->shouldReceive('getClass')->andReturn('Egulias\ListenersDebug\Tests\Listener\Listener');
 
-        $containerMock = $this->getMockBuilder('Symfony\Component\DependencyInjection\ContainerBuilder')
-            ->disableOriginalConstructor()
-            ->getMock();
+        $containerMock = m::mock('Symfony\Component\DependencyInjection\ContainerBuilder');
+        $containerMock->shouldReceive('getDefinitions')->once()->andReturn(array($defMock));
+        $containerMock->shouldReceive('findTaggedServiceIds')->once()->andReturn(array(array($eventData['config'])));
+        $containerMock->shouldReceive('hasDefinition')->andReturn(true);
+        $containerMock->shouldReceive('getDefinition')->once()->andReturn($defMock);
 
-        $containerMock->expects($this->once())->method('getDefinitions')
-            ->will($this->returnValue(array($defMock)));
-
-        $containerMock->expects($this->once())->method('findTaggedServiceIds')
-            ->will($this->returnValue(array(array($eventData['config']))));
-
-        $containerMock->expects($this->any())->method('hasDefinition')->will($this->returnValue(true));
-        $containerMock->expects($this->once())->method('getDefinition')->will($this->returnValue($defMock));
         $fetcher = new ListenerFetcher($containerMock);
         $listeners = $fetcher->fetchListeners();
 
@@ -56,7 +46,6 @@ class ListenerFetcherTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($eventData['expectations']['type'], $listeners[0][4]);
         $this->assertEquals($eventData['expectations']['priority'], $listeners[0][3]);
         $this->assertEquals('Egulias\ListenersDebug\Tests\Listener\Listener', $listeners[0][5]);
-
     }
 
     public function eventListenerDataProvider()
